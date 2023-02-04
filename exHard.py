7256af85f284fc64fcca13152061b27ba4dadbec
LooperCode <savchenkodkz@gmail.com>
2023-02-05 02:53:08 +0500

    first com

---
+++ exHard.py
@@ -0,0 +1,33 @@
+import random
+def Createlist():
+    n = int(input("Число: "))
+    listRnd = []
+    for i in range(n-1):
+        listRnd.append(random.randint(1, 11)) 
+    return listRnd
+
+def Maxincrement(listRnd):
+    minPos = maxPos = tempMin = tempMax = 0
+    minCount = maxCount = 0
+    for i in range(len(listRnd)):
+        index = 1
+        tempMin = listRnd[i]
+        while listRnd[i]+index in listRnd:
+            tempMax = listRnd[i]+index
+            minCount = index
+            index += 1
+        if minCount > maxCount:
+            maxCount = minCount
+            maxPos = tempMax
+            minPos = tempMin
+    return minPos, maxPos        
+
+
+                   
+                        
+
+listRnd = Createlist()
+print(listRnd)
+print(Maxincrement(listRnd))
+
+    
